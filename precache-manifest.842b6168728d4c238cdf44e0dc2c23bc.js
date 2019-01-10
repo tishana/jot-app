191380efa0dcaf4dfeebf1568e3b3c3171cdb48f
Tishana <tishana.trainor@gmail.com>
2019-01-09 19:23:44 -0500

    Updates

---
+++ precache-manifest.842b6168728d4c238cdf44e0dc2c23bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00778ab0d030de6cc8c",
+    "url": "/jot-app/notes/static/js/runtime~main.e00778ab.js"
+  },
+  {
+    "revision": "10d2e315d7ce8446257c",
+    "url": "/jot-app/notes/static/js/main.10d2e315.chunk.js"
+  },
+  {
+    "revision": "2891c8271e42ad894a80",
+    "url": "/jot-app/notes/static/js/1.2891c827.chunk.js"
+  },
+  {
+    "revision": "10d2e315d7ce8446257c",
+    "url": "/jot-app/notes/static/css/main.472acdf4.chunk.css"
+  },
+  {
+    "revision": "f549be2b7cd518ce5b3527f342fcfa76",
+    "url": "/jot-app/notes/index.html"
+  }
+];
